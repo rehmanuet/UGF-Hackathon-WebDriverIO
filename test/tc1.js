3738c8d076a457e4abf079ae8727b50071ce9fc7
Abdur Rehman <rehmanuet@yahoo.com>
2020-05-31 17:01:25 +0500

    v2

--- test/tc1.js
+++ test/tc1.js
@@ -17,20 +17,25 @@ describe('Task 1 - Header location', function() {
     
     it('Search field should be displayed',  () => {
 
-    let SearchBar = $('#DIV__customsear__41').isDisplayedInViewport();
-    console.log("Search Bar= "+SearchBar); 
+    // let SearchBar = $('#DIV__customsear__41').isDisplayedInViewport();
+    // console.log("Search Bar= "+SearchBar); 
     console.log( browser.capabilities.browserName)
-     let SearchBar_ISDISOLAYED = $('#DIV__customsear__41').isDisplayed();
+      let SearchBar_ISDISOLAYED = $('#DIV__customsear__41').isDisplayed();
       console.log("Search Bar_ IsDisplaced= "+SearchBar_ISDISOLAYED); 
-
-      console.log(browser)
-      assert.equal(Utils.hackathonReporter(1, "Search field is displayed", 'DIV__customsear__41', SearchBar),true);
+      assert.equal(Utils.hackathonReporter(1, "Search field is displayed", browser.capabilities.browserName, 'DIV__customsear__41', SearchBar_ISDISOLAYED),true);
   
     });
-    xit('Wish List should be displayed', () => {
+    it('Wishlist Icon should be displayed', () => {
 
-        let isDisplayedInViewport = $('#A__wishlist__52').isDisplayedInViewport();
+        let isDisplayedInViewport = $('#A__wishlist__52').isDisplayed();
         console.log("Wishlist= "+isDisplayedInViewport);
-        assert.equal(Utils.hackathonReporter(1, "Wish List should be displayed", '#A__wishlist__52', isDisplayedInViewport),true)[0];
+        assert.equal(Utils.hackathonReporter(1, "Wish List should be displayed", browser.capabilities.browserName, '#A__wishlist__52', isDisplayedInViewport),true);
 });
+it('Categories should be displayed', () => {
+
+  let categories = $('#UL____21').isDisplayed();
+  console.log("Categories= "+categories);
+  assert.equal(Utils.hackathonReporter(1, "Categories should be displayed", browser.capabilities.browserName, '#NAV__colxlcollg__10', categories),true);
+});
+
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
-const browsers = "Firefox";
 const viewport = "1200x700";
 const device = "Laptop";
 
 
 class FileUtils {
 
-    hackathonReporter(task, testName, domId, comparisonResult) {
+    hackathonReporter(task, testName,browsers, domId, comparisonResult) {
         fs.appendFileSync('Traditional-V1-TestResults.txt', `"Task: ${task}, Test Name: ${testName}, DOM Id: ${domId}, Browser: ${browsers}, Viewport: ${viewport}, Device: ${device}, Status: ${(comparisonResult ? "Pass" : "Fail")}\n`);
         return comparisonResult;
     }
